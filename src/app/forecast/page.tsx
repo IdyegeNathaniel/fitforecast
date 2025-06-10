86c9795f13af12cc87e618515e2ca650aab1c6c1
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-06-10 01:38:16 +0100

    Update

--- src/app/forecast/page.tsx
+++ src/app/forecast/page.tsx
@@ -1,16 +1,39 @@
+"use client"
+
 import Button from "@/components/Button"
+import { useState } from "react"
 import { HiSearch } from "react-icons/hi"
+import Cloud from '@/assets/cloud.png'
+import Image from "next/image"
 
 export default function Forecast() {
+
+    const [location, setLocation] = useState("");
+
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => { setLocation(e.target.value) };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => { e.preventDefault() } 
+
     return <section className="w-full py-20">
-        <div className="flex flex-col items-center justify-center mx-10">
-            <h1 className=" font-bold text-center text-base md:text-5xl font-bold leading-snug mb-3">Search for Weather and Get Fit Recommendation!</h1>
+        <div className="flex flex-col items-center justify-center mx-10 font-inter">
+            <h1 className=" font-bold text-center text-base md:text-5xl leading-snug mb-3">Search for Weather and Get Fit Recommendations!</h1>
             <p className="text-gray-500 text-sm md:text-base">Find Out What to Wear for Every Weather</p>
-            <div className="w-2/4 flex items-center justify-center my-5">
-                <input type="text" placeholder="Input Location" className="w-full text-sm md: text-xl p-2 border border-gray-500 rounded-l-md outline-none " />
-                <Button variant="secondary" size="md" className="md:p-4">
-                    <HiSearch className="font-extrabold" />
-                </Button>
+            <form onSubmit={handleSubmit} className="w-2/4 flex items-center justify-center my-5">
+                
+                    <input type="text" placeholder="Input Location" value={location} onChange={handleInput} />
+                    <datalist />
+                    <Button type="submit" variant="secondary" size="md" className="md:p-4">
+                        <HiSearch className="font-extrabold" />
+                    </Button>
+                
+            </form>
+
+            {/* WEATHER */}
+
+            <div className="flex flex-col items-center justify-center mt-5">
+                <Image src={Cloud} className='w-25' alt="weather-icon" />
+                <h3 className="text-6xl text-center">30 <sup>o</sup>C</h3>
+                <h3 className="text-4xl text-center">Lagos</h3>
             </div>
         </div>
     </section>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,26 @@
+"use client"
+
 import Image from "next/image"
 import Logo from "@/assets/FitForecast.png"
+import { useState } from "react"
+import { FaMoon, FaSun } from "react-icons/fa"
 
 const Header: React.FC = () => {
+
+  const [dark, setDark] = useState(true)
+
   return (
-    <header className="w-full flex items-center justify-start h-20 border-b border-gray-300 shadow-md">
+    <header className="w-full flex items-center justify-between h-20 border-b border-gray-300 shadow-md">
       <nav className="mx-10">
-        <Image src={Logo} className="w-auto h-15" alt="LOGO"/>
+        <Image src={Logo} className="w-auto h-15" alt="LOGO" />
       </nav>
+      <nav className="mx-10">
+        {
+          dark ? <FaSun className="text-nature-green" /> : <FaMoon className="text-nature-green" />
+        }
+      </nav>
+
+
     </header>
   )
 }
